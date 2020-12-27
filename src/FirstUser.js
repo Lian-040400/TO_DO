9e8ba9eec3d8dfde1908ae64542caa8222f29323
Lianna <xacatryanlianna630@gmail.com>
2020-12-27 21:17:38 +0400

    homeWork1

---
+++ src/FirstUser.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function FirstUser(props) {
+    return(
+    <span>{props.name} {props.surname} </span>
+    );
+}
+export {FirstUser}  ;
---
+++ src/SecondUser.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function SecondUser(props) {
+    return(
+    <span>{props.name} {props.surname} </span>
+    );
+}
+export default SecondUser ;
